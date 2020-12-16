721412834c1c50fa2fe706c0c09e944dbb6445f8
ganesh <sgganesh@gmail.com>
2020-12-16 09:37:51 +0530

    first version

---
+++ compiler-exercise-before-csharp/Compiler.cs
@@ -0,0 +1,67 @@
+// This code works, but follows procedural approach
+// Task: Convert it to Object Oriented approach and apply Composite pattern
+
+using System;
+
+namespace Compiler.Before
+{
+    internal class Expr
+    {
+        private readonly Expr _left;
+        private readonly Expr _right;
+        private readonly string _value;
+
+        public Expr(Expr left, string value, Expr right)
+        {
+            _left = left;
+            _value = value;
+            _right = right;
+        }
+
+        public void GenCode()
+        {
+            if (_left == null && _right == null)
+            {
+                Console.WriteLine("ldc.i4.s " + _value);
+            }
+            else
+            {
+                // its an intermediate node 
+                _left.GenCode();
+                _right.GenCode();
+                switch (_value)
+                {
+                    case "+":
+                        Console.WriteLine("add");
+                        break;
+                    case "-":
+                        Console.WriteLine("sub");
+                        break;
+                    case "*":
+                        Console.WriteLine("mul");
+                        break;
+                    case "/":
+                        Console.WriteLine("div");
+                        break;
+                    default:
+                        Console.WriteLine("Not implemented yet!");
+                        break;
+                }
+            }
+        }
+    }
+
+    internal class CompilerMain
+    {
+        public static void Main()
+        {
+            // ((10 * 20) + 30)  
+            var expr = new Expr(
+                new Expr(
+                    new Expr(null, "10", null), "*", new Expr(null, "20", null)),
+                "+",
+                new Expr(null, "30", null));
+            expr.GenCode();
+        }
+    }
+}
